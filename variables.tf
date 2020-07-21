fa8615a2d460b28574ddd502a7f113b1647e95f0
Sayam Masood <mixpix3ls@protonmail.com>
2020-07-21 09:19:40 -0500

    Remove default token and formatting

--- variables.tf
+++ variables.tf
@@ -1,24 +1,29 @@
 # Variables for VCD
 
 variable "vcd_url" {
+  type        = string
   description = "VCD URL"
   default     = "https://vcloud.wwtatc.local/api"
 }
 variable "org" {
+  type        = string
   description = "VCD Organization"
   default     = "vCD-Perm"
 }
 variable "vdc" {
+  type        = string
   description = "VCD VDC"
   default     = "vCD-Perm-vApps"
 }
 
 variable "vault_addr" {
+  type        = string
   description = "Vault Address"
   default     = ""
 }
 
 variable "vcduser" {
+  type    = string
   default = ""
 }
 
@@ -28,13 +33,16 @@ variable "vcdpassword" {
 }
 
 variable "token_id" {
-  default = "s.ntT21pt6UmoFSySu546JmLSZ"
+  type    = string
+  default = ""
 }
 
 variable "catalog" {
+  type    = string
   default = "Perm-vApps"
 }
 
 variable "template_name" {
+  type    = string
   default = "Nginx-Lab-Module-2.v2"
 }
